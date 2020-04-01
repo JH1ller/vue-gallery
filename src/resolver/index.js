9e1907870cf4a7c017b0aeebb1ff906fa4190e38
Justin Hiller <justin.hiller.privat@googlemail.com>
2020-04-01 12:07:29 +0200

    deleted accidentally generated js files

--- src/resolver/index.js
+++
@@ -1,6 +0,0 @@
-export const basicResolver = async (to, from, next) => {
-    /* const res = await store.dispatch('loadZones');
-    if (res) next(); */
-    next();
-};
-//# sourceMappingURL=index.js.map
--- src/store/mutation-types.js
+++
@@ -1,4 +0,0 @@
-export const RESET_STATE = 'RESET_STATE';
-export const SHOW_TOAST = 'SHOW_TOAST';
-export const HIDE_TOAST = 'HIDE_TOAST';
-//# sourceMappingURL=mutation-types.js.map
